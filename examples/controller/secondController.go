fc40a33e4a9efe9fee759981d69445ce08c110d2
Drumil Patel <42701709+drumilpatel2000@users.noreply.github.com>
2019-10-21 02:53:05 +0530

    Removing name initialization using function (#10)

--- examples/controller/secondController.go
+++ examples/controller/secondController.go
@@ -11,13 +11,13 @@ type SecondController struct {
 }
 
 func (c *SecondController) Index(w http.ResponseWriter, r *http.Request, params url.Values) {
-	c.Initiliaze(w, r, "SecondController")
+	c.Initiliaze(w, r)
 	c.RenderText(c.Name + " index is working")
 	c.Terminate()
 }
 
 func (c *SecondController) List(w http.ResponseWriter, r *http.Request, params url.Values) {
-	c.Initiliaze(w, r, "SecondController")
+	c.Initiliaze(w, r)
 	c.RenderText(c.Name + " list is working")
 	c.Terminate()
 }
